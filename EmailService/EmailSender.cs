7f2a8d8d77cf60938f45409c06d7105ee6c71b41
nirzaf <mfmfazrin1986@gmail.com>
2022-05-13 15:18:09 +0530

    $"<h2 style='color:black;'>{message.Content}</h2>"

--- EmailService/EmailSender.cs
+++ EmailService/EmailSender.cs
@@ -1,4 +1,5 @@
-﻿using MailKit.Net.Smtp;
+﻿using System;
+using MailKit.Net.Smtp;
 using MimeKit;
 using System.IO;
 using System.Linq;
@@ -27,20 +28,18 @@ namespace EmailService
             emailMessage.From.Add(new MailboxAddress(_emailConfig.UserName, _emailConfig.From));
             emailMessage.To.AddRange(message.To);
             emailMessage.Subject = message.Subject;
-
-            var bodyBuilder = new BodyBuilder { HtmlBody = string.Format("<h2 style='color:red;'>{0}</h2>", message.Content) };
+            var bodyBuilder = new BodyBuilder { HtmlBody = $"<h2 style='color:black;'>{message.Content}</h2>" };
 
             if (message.Attachments != null && message.Attachments.Any())
             {
-                byte[] fileBytes;
                 foreach (var attachment in message.Attachments)
                 {
+                    byte[] fileBytes;
                     using (var ms = new MemoryStream())
                     {
                         attachment.CopyTo(ms);
                         fileBytes = ms.ToArray();
                     }
-
                     bodyBuilder.Attachments.Add(attachment.FileName, fileBytes, ContentType.Parse(attachment.ContentType));
                 }
             }
@@ -51,26 +50,22 @@ namespace EmailService
 
         private async Task SendAsync(MimeMessage mailMessage)
         {
-            using (var client = new SmtpClient())
+            using var client = new SmtpClient();
+            try
             {
-                try
-                {
-                    await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, true);
-                    client.AuthenticationMechanisms.Remove("XOAUTH2");
-                    await client.AuthenticateAsync(_emailConfig.UserName, _emailConfig.Password);
-
-                    await client.SendAsync(mailMessage);
-                }
-                catch
-                {
-                    //log an error message or throw an exception, or both.
-                    throw;
-                }
-                finally
-                {
-                    await client.DisconnectAsync(true);
-                    client.Dispose();
-                }
+                await client.ConnectAsync(_emailConfig.SmtpServer, _emailConfig.Port, true);
+                client.AuthenticationMechanisms.Remove("XOAUTH2");
+                await client.AuthenticateAsync(_emailConfig.UserName, _emailConfig.Password);
+                await client.SendAsync(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                await client.DisconnectAsync(true);
+                client.Dispose();
             }
         }
     }
